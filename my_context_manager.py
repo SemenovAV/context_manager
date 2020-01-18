8bfa5ad8c03362018d9f466ba26225fcf65bf26c
Aleksey Semenov <7.on.off@bk.ru>
2020-01-19 01:59:39 +0300

    Рефакторинг.
    Убирает лишнее.

--- my_context_manager.py
+++ my_context_manager.py
@@ -2,16 +2,12 @@ from datetime import datetime
 
 
 class MyContextManager:
-    def __init__(self, script):
-        self.script = script
 
     def __enter__(self):
         self.start = datetime.now()
         print('Код запущен:', self.start)
-        return self.script
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop = datetime.now()
         print('Код остановлен:', self.stop)
         print('Время работы:', self.stop - self.start)
-        return
