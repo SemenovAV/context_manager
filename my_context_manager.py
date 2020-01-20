27c8c43ab781cd6133a64486a10c1aed4d1d4a33
Aleksey Semenov <7.on.off@bk.ru>
2020-01-20 10:38:26 +0300

    fix

--- my_context_manager.py
+++ my_context_manager.py
@@ -1,27 +1,28 @@
 from datetime import datetime
 
+'''Замеряет скорость работы кода. Результат печатает в консоль и логирует в файл.'''
+
 
 class MyContextManager:
     def __init__(self, log_file_path):
         self.path = log_file_path
-        self.timestamp = lambda: datetime.now()
 
     def __enter__(self):
         self.log = open(self.path, 'a', encoding='utf-8')
-        self.start = datetime.now()
+        self.start_time = datetime.now()
         self.set_event('Код запущен.')
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.stop = datetime.now()
+        self.stop_time = datetime.now()
         if exc_tb:
             self.set_event(f'Ошибка: {exc_val}')
             self.set_event('Код остановлен.')
         else:
             self.set_event('Код остановлен.')
-            self.time = self.stop - self.start
+            self.time = self.stop_time - self.start_time
             self.set_event(f'Время работы: {self.time}')
 
     def set_event(self, message):
-        event = f'{self.timestamp()}: {message}'
+        event = f'{datetime.now()}: {message}'
         self.log.write(event + '\n')
         print(event)
